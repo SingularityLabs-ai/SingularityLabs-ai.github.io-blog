3062bd3334551152dfcf4339dd50d6e577778945
ishan dutta <ishandutta2007@gmail.com>
2023-10-20 13:08:14 +0530

    tailwindcss

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,7 @@ module.exports = {
   parser: 'postcss-scss',
   plugins: [
     require('postcss-import'),
+    require('tailwindcss'),
     require('autoprefixer'),
     ...(process.env.JEKYLL_ENV == "production"
       ? [require('cssnano')({ preset: 'default' })]
